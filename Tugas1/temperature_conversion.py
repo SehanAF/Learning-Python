e437ba0e1d1f4069e7bd6ce77248a5246614529c
SehanAF <sehanaf4@gmail.com>
2024-02-15 23:15:50 +0700

    Membersihkan kode dari variabel yang tidak digunakan serta menyertakan keterangan tambahan pada pesan output.

--- Tugas1/temperature_conversion.py
+++ Tugas1/temperature_conversion.py
@@ -1,13 +1,9 @@
 reamur = float(input("Masukan suhu celcius: "))
 
-celcius = 0.0
-fahrenheit = 0.0
-kelvin = 0.0
-
 formulaCelcius = 5 / 4 * reamur
 formulaFahrenheit = 9 / 4 * reamur + 32
 formulaKelvin = 5 / 4 * reamur + 273.15
 
-print("Suhu Reamur %.2f -> Suhu Celcius: %.2f" % (reamur, formulaCelcius) )
-print("Suhu Reamur %.2f -> Suhu Fahrenheit: %.2f" % (reamur, formulaFahrenheit))
-print("Suhu Reamur %.2f -> Suhu Kelvin: %.2f" % (reamur, formulaKelvin))
+print("Suhu Reamur %.2f -> Suhu Celcius: %.2f Derajat Celcius" % (reamur, formulaCelcius) )
+print("Suhu Reamur %.2f -> Suhu Fahrenheit: %.2f Derajat Fahrenheit" % (reamur, formulaFahrenheit))
+print("Suhu Reamur %.2f -> Suhu Kelvin: %.2f Derajat Kelvin" % (reamur, formulaKelvin))
--- Tugas1/triangle_operations.py
+++ Tugas1/triangle_operations.py
@@ -2,11 +2,11 @@ alas = float(input("Masukan angka alas: "))
 tinggi = float(input("Masukan angka tinggi: "))
 
 sisiA = 20
-sisiB = 12
+sisiB = 15
 sisiC = 10
 
 rumusLuas = 1 / 2 * alas * tinggi
 rumusKelilingSegitiga = sisiA + sisiB + sisiC
 
-print("hasil luas %.0f" % rumusLuas)
-print("hasil keliling %.0f" % rumusKelilingSegitiga)
+print("Hasil luas segitiga dengan alas %.0f cm dan tinggi %.0f cm adalah %.2f cm." % (alas, tinggi, rumusLuas))
+print("Hasil keliling segitiga dengan sisi A %.0f cm, sisi B %.0f cm, dan sisi C %.0f cm adalah %.2f cm." % (sisiA, sisiB, sisiC, rumusKelilingSegitiga))
