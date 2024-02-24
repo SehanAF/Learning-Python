28ae21918454a83a5bf2665c54143a27ed88785d
SehanAF <sehanaf4@gmail.com>
2024-02-24 14:19:27 +0700

    revisi penamaan variabel dan revisi logic dalam perhitungan diskon.

--- Tugas 2/hitung_kembalian_diskon.py
+++ Tugas 2/hitung_kembalian_diskon.py
@@ -1,10 +1,11 @@
 uang_belanja = float(input("Masukan jumlah uang belanja: "))
 total_barang = float(input("Masukan jumlah total harga barang: "))
 
-total_belanja = uang_belanja - total_barang
+sisa_kembalian = uang_belanja - total_barang
 
 if total_barang > 70.000:
-    diskon = total_barang - 0.1
-    print(f"Sisa kembalian anda setelah diskon adalah: {diskon:.3f}")
+    diskon = total_barang * 0.10 
+    sisa_kembalian += diskon
+    print(f"Sisa kembalian anda setelah diskon adalah: {sisa_kembalian:.3f}")
 else:     
-    print(f"Sisa Kembalian anda adalah: {total_belanja:.3f} rupiah.")
+    print(f"Sisa Kembalian anda adalah: {sisa_kembalian:.3f} rupiah.")
